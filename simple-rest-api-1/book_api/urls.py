53564765b58cdc070d44120d5f15ce1aec4afb6a
YoungChulShin <go1323@gmail.com>
2022-11-22 23:48:25 +0900

    method -> classview로 변경 (메서드 추가)

--- simple-rest-api-1/book_api/urls.py
+++ simple-rest-api-1/book_api/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from book_api.views import BookList
+from book_api.views import BookCreate, BookList
 
 urlpatterns = [
     # path('', book_create),
     # path('list/', book_list),
     # path('<int:pk>', book),
-
+    path('', BookCreate.as_view()),
     path('list/', BookList.as_view()),
 ]
--- simple-rest-api-1/book_api/views.py
+++ simple-rest-api-1/book_api/views.py
@@ -55,6 +55,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from book_api.models import Book
 from book_api.serializer import BookSerializer
+from rest_framework import status
 
 class BookList(APIView):
     def get(self, request):
@@ -62,3 +63,37 @@ class BookList(APIView):
         serialzier = BookSerializer(books, many=True)
         return Response(serialzier.data)
 
+class BookCreate(APIView):
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Book(APIView):
+    def get_book_by_pk(self, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+        except:
+            return Response({
+                'error': 'Book does not exist'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        book = self.get_book_by_pk(pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        book = self.get_book_by_pk(pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        book = self.get_book_by_pk(pk)
+        book.delete() 
+        return Response(status=status.HTTP_204_NO_CONTENT)
